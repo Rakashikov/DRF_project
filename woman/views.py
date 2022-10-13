9249bd213f5d4e14471c2c0a019810ca12b3b059
Evgeniy Rakashikov <evgeniyhd@gmail.com>
2022-10-13 13:59:32 +0600

    without serializer

--- woman/views.py
+++ woman/views.py
@@ -1,10 +1,27 @@
+from django.forms import model_to_dict
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import Woman
 from .serializers import WomanSerializer
 
 
-class WomanApiView(generics.ListAPIView):
-    queryset = Woman.objects.all()
-    serializer_class = WomanSerializer
+class WomanApiView(APIView):
+    def get(self, req):
+        lst = Woman.objects.all()
+        return Response({'posts': list(lst.values())})
+
+    def post(self, req):
+        post_new = Woman.objects.create(
+            title=req.data['title'],
+            content=req.data['content'],
+            category_id=req.data['category_id']
+        )
+        return Response({'post': model_to_dict(post_new)})
+
+
+# class WomanApiView(generics.ListAPIView):
+#     queryset = Woman.objects.all()
+#     serializer_class = WomanSerializer
