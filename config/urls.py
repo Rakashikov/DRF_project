004e638e0679a07fb23d1f4136374a17e8866423
Evgeniy Rakashikov <evgeniyhd@gmail.com>
2022-11-12 00:53:30 +0600

    Session auth

--- config/urls.py
+++ config/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from woman.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/drf-auth/', include('rest_framework.urls')),
     path('api/v1/woman/', WomanAPIListCreateView.as_view()),
     path('api/v1/woman/<int:pk>/', WomanAPIUpdateView.as_view()),
     path('api/v1/womandelete/<int:pk>/', WomanAPIDestroyView.as_view()),
